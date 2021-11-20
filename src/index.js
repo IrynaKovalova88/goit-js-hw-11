a270a2880a94fc9b01e95d63e55ac05b49ec5d16
IrynaKovalova88 <Avril19882012@gmail.com>
2021-11-20 05:21:25 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
-import './sass/main.scss';
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import PhotosPixabay from './js/search-photo';
+import photoCard from './template/photo-card.hbs';
+
+const photosPixabay = new PhotosPixabay();
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more-btn')
+}
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+function onSearch(e) {
+  e.preventDefault();
+  photosPixabay.query = e.currentTarget.elements.searchQuery.value.trim();
+
+  if (photosPixabay.query === '') {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    refs.gallery.innerHTML = '';
+    return Notify.failure(`Please, type your search query`);
+  }
+  photosPixabay.resetPage();
+  photosPixabay.searchPhotos().then(({hits, totalHits}) => {
+    refs.gallery.innerHTML = '';
+    if (hits.length === 0) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      return Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+    }
+    Notify.info(`Hooray! We found ${totalHits} images.`);
+    renderPhotos(hits);
+    lightboxPhotos();
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  })
+}
+
+function onLoadMoreBtn() {
+  photosPixabay.searchPhotos().then(({ hits, totalHits, page, perPage }) => {
+    renderPhotos(hits);
+    lightboxPhotos();
+    scroll();
+    if (page < totalHits / perPage) {
+      refs.loadMoreBtn.classList.add("is-hidden");
+      return Notify.info(`We're sorry, but you've reached the end of search results.`);
+    }
+  })
+    }
+
+function renderPhotos(hits) {
+    refs.gallery.insertAdjacentHTML('beforeend', photoCard(hits));
+}
+
+function lightboxPhotos() {
+  const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
+}
+
+function scroll () {
+  const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 8,
+  behavior: "smooth",
+});
+}
---
+++ src/js/search-photo.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export default class PhotosPixabay {
+    constructor() {
+        this.searchQuery = "";
+        this.page = 1;
+        this.perPage = 40;
+        this.totalHits = 0;
+    }
+    async searchPhotos() {
+        const KEY = '24399627-fe2224483e9e3196e9df20926';
+    axios.defaults.baseURL = 'https://pixabay.com/api';
+  try {
+        const resp = await axios.get(`?key=${KEY}&q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&image_type=photo&orientation=horizontal&safesearch=true`);
+        this.incrementPage();
+        return resp.data;
+        } catch(error) {
+      return error;
+        }
+
+    }
+
+    incrementPage() {
+        this.page +=1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
