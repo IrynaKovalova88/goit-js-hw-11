5d63ff8ac0510d53b5190af901c77f4336316dd3
IrynaKovalova88 <Avril19882012@gmail.com>
2021-11-21 03:49:44 +0200

    fix

--- src/js/search-photo.js
+++ src/js/search-photo.js
@@ -5,7 +5,7 @@ export default class PhotosPixabay {
         this.searchQuery = "";
         this.page = 1;
         this.perPage = 40;
-        this.totalHits = 0;
+        this.totalHits = null;
     }
     async searchPhotos() {
         const KEY = '24399627-fe2224483e9e3196e9df20926';
